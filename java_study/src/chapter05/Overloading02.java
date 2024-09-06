e078b50b788236673c7da1ff57f7e45476030e55
devgiants75 <devgiants75@naver.com>
2024-09-06 11:39:48 +0900

    20240906

--- java_study/src/chapter05/Overloading02.java
+++ java_study/src/chapter05/Overloading02.java
@@ -9,11 +9,29 @@ class Restaurant {
 	// >> 메뉴 이름, 수량 제공(int quantity)
 	// >> 메뉴 이름, 특별 요청 사항 제공(String specialRequests)
 	// >> 메뉴 이름, 수량 제공, 특별 요청 사항 제공
+	void placeOrder(String dish) {
+		System.out.println(dish);
+	}
+	void placeOrder(String dish, int quantity) {
+		System.out.println(dish + quantity);
+	}
+	void placeOrder(String dish, String specialRequests) {
+		System.out.println(dish + specialRequests);
+	}
+	void placeOrder(String dish, int quantity, String specialRequests) {
+		System.out.println(dish + quantity + specialRequests);
+	}
+	
+	// >> 각 메서드에서 매개변수의 데이터값을 출력
 }
 
 public class Overloading02 {
 	public static void main(String[] args) {
 		// 인스턴스 생성
-		// 
+		Restaurant order1 = new Restaurant();
+		order1.placeOrder("햄버거");
+		order1.placeOrder("피자", 2);
+		order1.placeOrder("파스타", "치즈 많이");
+		order1.placeOrder("샐러드", 2, "드레싱 없이");				
 	}
 }
