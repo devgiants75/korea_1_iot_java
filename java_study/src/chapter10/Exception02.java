22aa57fce8ec1b2954e5a8fdbe9eca16f2cab9a1
devgiants75 <devgiants75@naver.com>
2024-09-09 15:48:53 +0900

    20240909

--- java_study/src/chapter10/Exception02.java
+++ java_study/src/chapter10/Exception02.java
@@ -30,13 +30,10 @@ public class Exception02 {
 			System.out.println("입력하신 숫자는 " + number + "입니다.");
 		} catch (InputMismatchException e) {
 			System.out.println("정수가 아닌 다른 타입의 입력이 작성됨");
+			// e.getMessage()
+			// : 위의 결과값이 null인 경우 해당 예외에 대한 메시지가 담겨있지 X
 		} finally {
 			sc.close();
 		}
-		
-		
-		
-		
-		
 	}
 }
