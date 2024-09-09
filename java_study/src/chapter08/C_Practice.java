048828238c0cb4795c10db73c662be86558815ec
devgiants75 <devgiants75@naver.com>
2024-09-09 11:49:56 +0900

    20240909

--- java_study/src/chapter08/C_Practice.java
+++ java_study/src/chapter08/C_Practice.java
@@ -7,6 +7,25 @@ package chapter08;
 
 // Cat, Dog 클래스 - Animal 클래스를 상속
 // : sound 메서드를 오버라이딩 >> 각각 "meow", "bark" 출력
+class Animal {
+	void sound() {
+		System.out.println("The animal makes a sound");
+	}
+}
+
+class Cat extends Animal {
+	@Override
+	void sound() {
+		System.out.println("Meow");
+	}
+}
+
+class Dog extends Animal {
+	@Override
+	void sound() {
+		System.out.println("Bark");
+	}
+}
 
 // === instanceof 연산자 === 
 // AnimalChecker 클래스 선언
@@ -15,8 +34,36 @@ package chapter08;
 // : instanceof 연산자를 사용하여 매개변수가 Cat과 Dog 중 어느 인스턴스인지 확인
 // : Cat > 'cat'출력 / Dog > 'dog'출력 / 둘 다 X > 'Unknown animal type'
 
+class AnimalChecker {
+	void checkAnimalType(Animal animal) {
+		if (animal instanceof Cat) {
+			System.out.println("cat");
+		} else if (animal instanceof Dog) {
+			System.out.println("dog");
+		} else {
+			System.out.println("Unknown animal type");
+		}
+	}
+}
+
 public class C_Practice {
 	public static void main(String[] args) {
+		// Animal, Cat, Dog 클래스의 객체를 Animal 타입으로 저장
+		AnimalChecker animalChecker = new AnimalChecker();
 		
+		Animal myPet1 = new Animal();
+		animalChecker.checkAnimalType(myPet1); // Unknown animal type
+		
+		// 강제 형 변환의 경우
+		// >> 자동 형 변환으로 상속받은 클래스 형태를 다시 
+		//    인스턴스의 타입으로 변환 할 경우에 사용
+//		Dog myPetDog = (Dog) myPet1; - Error
+//		animalChecker.checkAnimalType(myPetDog);
+		
+		Animal myPet2 = new Cat();
+		animalChecker.checkAnimalType(myPet2); // cat
+
+		Animal myPet3 = new Dog();
+		animalChecker.checkAnimalType(myPet3); // dog
 	}
 }
