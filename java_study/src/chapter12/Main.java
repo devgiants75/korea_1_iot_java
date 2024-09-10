db2306f8042b053e4f3bd24dd478624a107f125d
devgiants75 <devgiants75@naver.com>
2024-09-10 15:44:17 +0900

    20240910

--- java_study/src/chapter12/Main.java
+++ java_study/src/chapter12/Main.java
@@ -6,5 +6,14 @@ public class Main {
 	public static void main(String[] args) {
 		BookView bookView = new BookView();
 		bookView.showMenu();
+		
+		int[] numbers = {1, 2, 3, 4, 5};
+		// for-each문
+		// for (배열요소타입: 배열명) {
+		
+		// }
+		for (int num: numbers) {
+			System.out.println(num);
+		}
 	}
 }
