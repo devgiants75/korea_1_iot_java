90b8e1b611f733a5cd31e1c7863f75a302038b15
devgiants75 <devgiants75@naver.com>
2024-09-12 10:00:25 +0900

    20240912

--- java_study/src/chapter15/BookAlreadyBorrowedException.java
+++ java_study/src/chapter15/BookAlreadyBorrowedException.java
@@ -1,5 +1,9 @@
 package chapter15;
 
-public class BookAlreadyBorrowedException {
-
+// 사용자 정의 예외
+// : 책이 이미 대여 중일 때 발생하는 예외
+public class BookAlreadyBorrowedException extends RuntimeException {
+	public BookAlreadyBorrowedException(String message) {
+		super(message);
+	}
 }
